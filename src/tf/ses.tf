3a1b00a886bde3e3c52214f147e0332ec0b8ff8b
Levi Zitting <me@levizitting.com>
2026-07-22 07:18:43 -0700

    feat: add ses vdm with a transactional configuration set

--- src/tf/ses.tf
+++ src/tf/ses.tf
@@ -3,8 +3,41 @@ locals {
   ses_mail_from_domain = "bounce.${local.ses_email_identity}"
 }
 
+resource "aws_sesv2_account_vdm_attributes" "current" {
+  vdm_enabled = "ENABLED"
+
+  dashboard_attributes {
+    engagement_metrics = "DISABLED"
+  }
+
+  guardian_attributes {
+    optimized_shared_delivery = "DISABLED"
+  }
+}
+
+resource "aws_sesv2_configuration_set" "transactional" {
+  configuration_set_name = "transactional"
+
+  vdm_options {
+    dashboard_options {
+      engagement_metrics = "DISABLED"
+    }
+
+    guardian_options {
+      optimized_shared_delivery = "DISABLED"
+    }
+  }
+
+  tags = {
+    ManagedBy = "OpenTofu"
+  }
+
+  depends_on = [aws_sesv2_account_vdm_attributes.current]
+}
+
 resource "aws_sesv2_email_identity" "levizitting_com" {
-  email_identity = local.ses_email_identity
+  email_identity         = local.ses_email_identity
+  configuration_set_name = aws_sesv2_configuration_set.transactional.configuration_set_name
 
   tags = {
     ManagedBy = "OpenTofu"
